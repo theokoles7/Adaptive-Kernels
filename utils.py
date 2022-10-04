336055f19dae96fbeb9cb1511e99d9cf14ed8aef
PaperUnderReviewDeepLearning <91948725+PaperUnderReviewDeepLearning@users.noreply.github.com>
2022-10-04 10:32:55 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -35,6 +35,30 @@ def get_gaussian_filter(epoch_number,mean =1, kernel_size=3, sigma=2, channels=3
     elif kernel_number == 6:
         filter_list = ['bottomLeft', 'bottomRight']
         print ("Kernel Type 6: bottom-left, bottom-right")
+    elif kernel_number == 7:
+        filter_list = ['topLeft', 'bottomRight','static']
+        print ("Kernel Type 7: top-left, bottom-right","static")
+    elif kernel_number == 8:
+        filter_list = ['bottomLeft', 'topRight','static']
+        print("Kernel Type 8: bottom-left, top-right","static")
+    elif kernel_number== 9:
+        filter_list = ['topLeft', 'bottomLeft','static']
+        print ("Kernel Type 9: top-left, bottom-left","static")
+    elif kernel_number == 10:
+        filter_list = ['topRight', 'bottomRight','static']
+        print ("kernel Type 10: top-right, bottom-right","static")
+    elif kernel_number == 11:
+        filter_list = ['topLeft', 'topRight','static']
+        print ("Kernel Type 11: top-left, top-right","static")
+    elif kernel_number == 12:
+        filter_list = ['bottomLeft', 'bottomRight','static']
+        print ("Kernel Type 12: bottom-left, bottom-right","static")
+    elif kernel_number == 13:
+        filter_list = ['topLeft', 'bottomRight','bottomLeft', 'topRight']
+        print ("Kernel Type 13: top-left, bottom-right ,bottom-left, top-right")
+    elif kernel_number == 14:
+        filter_list = ['topLeft', 'bottomRight','bottomLeft', 'topRight','static']
+        print ("Kernel Type 14: top-left, bottom-right ,bottom-left, top-right, static")
 
 
 
@@ -55,7 +79,21 @@ def get_gaussian_filter(epoch_number,mean =1, kernel_size=3, sigma=2, channels=3
     
     5 = top left top right
     6 = bottom left bottom right
+    
+    7 = top left bottom right static
+    8 = bottom left top right static
+    
+    9 = top left bottom left static
+    10 = top right bottom right static
+    
+    11 = top left top right static
+    12 = bottom left bottom right static
+    
+    13 = top left bottom right bottom left top right
+    14 = top left bottom right bottom left top right
     '''
+
+
 
 
      #Exp 2 , 3 and 4 Kernel
@@ -72,18 +110,19 @@ def get_gaussian_filter(epoch_number,mean =1, kernel_size=3, sigma=2, channels=3
     #filter_list = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight', 'middle-left', 'middle-right', 'middle-top', 'middle-bottom']  # Exp 8
 
     #filter_name = filter_list[random.randint(0, 4)] # Exp 1
-    filter_name = filter_list[random.randint(0, 1)] # Exp 2 Kernel Set
+    #filter_name = filter_list[random.randint(0, 1)] # Exp 2 Kernel Set
     #filter_name = filter_list[0] # Exp 5~7
     #filter_name = filter_list[random.randint(0, 3)] #Exp 4 Kernel
     #filter_name = filter_list[random.randint(0, 7)] # Exp 8 kernel
 
 
+    filter_name = filter_list[random.randint(0,len(filter_list)-1)]
     x_coord = torch.arange(kernel_size)
     x_grid = x_coord.repeat(kernel_size).view(kernel_size, kernel_size)
     y_grid = x_grid.t()
     xy_grid = torch.stack([x_grid, y_grid], dim=-1).float()
 
-    if filter_name == 'forceG':
+    if filter_name == 'static':
         mean = 1
         sigma = 1
     else:
@@ -106,8 +145,11 @@ def get_gaussian_filter(epoch_number,mean =1, kernel_size=3, sigma=2, channels=3
     # Make sure sum of values in gaussian kernel equals 1.
     gaussian_kernel = gaussian_kernel / torch.sum(gaussian_kernel)
 
+    if filter_name == 'static':
+        cprint("static", "red", "on_white")
+        print(gaussian_kernel)
 
-    if filter_name == 'topLeft':
+    elif filter_name == 'topLeft':
         cprint("Top Left", "red", "on_white")
         print(gaussian_kernel)
 
@@ -132,78 +174,7 @@ def get_gaussian_filter(epoch_number,mean =1, kernel_size=3, sigma=2, channels=3
         cprint("Bottom Left", "red", "on_white")
         print(gaussian_kernel)
 
-    elif filter_name == 'middle-left':
-        temp = gaussian_kernel.clone()
-        gaussian_kernel[1][0] = temp[0][0]
-        gaussian_kernel[1][1] = temp[1][1]
-        gaussian_kernel[1][2] = temp[2][2]
 
-        gaussian_kernel[0][0] = temp[0][1]
-        gaussian_kernel[2][0] = temp[0][1]
-
-        gaussian_kernel[0][1] = temp[0][2]
-        gaussian_kernel[2][1] = temp[0][2]
-
-        gaussian_kernel[0][2] = temp[1][2]
-        gaussian_kernel[2][2] = temp[1][2]
-
-        cprint("middle left", "red","on_white")
-        print(gaussian_kernel)
-
-    elif filter_name == 'middle-right':
-        temp = gaussian_kernel.clone()
-        gaussian_kernel[1][2] = temp[0][0]
-        gaussian_kernel[1][1] = temp[1][1]
-        gaussian_kernel[1][0] = temp[2][2]
-
-        gaussian_kernel[0][2] = temp[0][1]
-        gaussian_kernel[2][2] = temp[0][1]
-
-        gaussian_kernel[0][1] = temp[0][2]
-        gaussian_kernel[2][1] = temp[0][2]
-
-        gaussian_kernel[0][0] = temp[1][2]
-        gaussian_kernel[2][0] = temp[1][2]
-
-        cprint("middle right", "red", "on_white")
-        print(gaussian_kernel)
-
-    elif filter_name == 'middle-top':
-        temp = gaussian_kernel.clone()
-        gaussian_kernel[0][1] = temp[0][0]
-        gaussian_kernel[1][1] = temp[1][1]
-        gaussian_kernel[2][1] = temp[2][2]
-
-        gaussian_kernel[0][0] = temp[0][1]
-        gaussian_kernel[0][2] = temp[0][1]
-
-        gaussian_kernel[1][0] = temp[0][2]
-        gaussian_kernel[1][2] = temp[0][2]
-
-        gaussian_kernel[2][0] = temp[1][2]
-        gaussian_kernel[2][2] = temp[1][2]
-
-        cprint("middle top", "red", "on_white")
-        print(gaussian_kernel)
-
-
-    elif filter_name == 'middle-bottom':
-        temp = gaussian_kernel.clone()
-        gaussian_kernel[2][1] = temp[0][0]
-        gaussian_kernel[1][1] = temp[1][1]
-        gaussian_kernel[0][1] = temp[2][2]
-
-        gaussian_kernel[2][0] = temp[0][1]
-        gaussian_kernel[2][2] = temp[0][1]
-
-        gaussian_kernel[1][0] = temp[0][2]
-        gaussian_kernel[1][2] = temp[0][2]
-
-        gaussian_kernel[0][0] = temp[1][2]
-        gaussian_kernel[0][2] = temp[1][2]
-
-        cprint("middle bottom", "red", "on_white")
-        print(gaussian_kernel)
 
 
 

59ef6a768b90da394766df54c349ccfbc99240fb
theokoles <gabrieltrahan777@gmail.com>
2023-07-19 21:33:39 -0500

    Refactors

--- arguments.py
+++ arguments.py
@@ -1,38 +1,160 @@
-import argparse
-import torch
+"""Parse and handle command line arguments."""
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default='../data')
-    parser.add_argument('--dataset', type=str, default='cifar10')
-    parser.add_argument('--log_name', type=str, default='cbs')
-    parser.add_argument('--alg', type=str, default='res', choices=['normal', 'vgg', 'res'])
-    parser.add_argument('--log_path', type=str, default='logs')
-    parser.add_argument('--no-cuda', action='store_true')
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--num_epochs', type=int, default=200)
-    parser.add_argument('--ssl', action='store_true')
-    parser.add_argument('--percentage', type=int, default=10)
-    parser.add_argument('--save_model', action='store_true')
-    parser.add_argument('--lr', type=float, default=1e-1)
+import argparse, torch
+
+def get_args() -> argparse.Namespace:
+    """Parse command line arguments.
+
+    Returns:
+        argparse.Namespace: Arguments Namespace
+    """
+    # Initialize parser
+    parser = argparse.ArgumentParser(
+        prog = 'hilo',
+        description = 'Fork of KernelSet, intended for studying efficacy of models on high or low resolution images'
+    )
+
+    # ARGUMENTS -----------------------------------------------------------------------------------
+    parser.add_argument(
+        '--data', 
+        type = str, 
+        default = '../data',
+        help = 'data source/location of datasets'
+    )
+
+    parser.add_argument(
+        '--dataset',
+        type = str,
+        choices = [
+            'mnist', 'cifar10', 'cifar100', 
+            'imagenet', 'svhn', 'caltech'
+        ],
+        default = 'cifar10',
+        help = 'target dataset'
+    )
+
+    parser.add_argument(
+        '--model', 
+        type = str, 
+        choices = ['normal', 'vgg', 'res'],
+        default = 'res',
+        help = 'choice of model'
+    )
+
+    parser.add_argument(
+        '--log_name', 
+        type = str, 
+        default = 'cbs',
+        help = 'name of logger'
+    )
+
+    parser.add_argument(
+        '--log_path', 
+        type = str, 
+        default = 'logs',
+        help = 'path at which log file will be created'
+    )
+
+    parser.add_argument(
+        '--no-cuda',
+        type =      bool,
+        action =    'store_true',
+        help = 'opt out of running on GPU'
+        )
+
+    parser.add_argument(
+        '--batch_size', 
+        type = int, 
+        default = 64,
+        help = 'specify batch size'
+    )
+
+    parser.add_argument(
+        '--num_epochs', 
+        type = int, 
+        default = 200,
+        help = 'specify number of epochs'
+    )
+
+    parser.add_argument(
+        '--lr', 
+        type = float, 
+        default = 1e-1,
+        help = 'specify learning rate'
+    )
+
+    parser.add_argument(
+        '--ssl', 
+        action = 'store_true',
+        help = 'SSL *****'
+    )
+
+    parser.add_argument(
+        '--percentage', 
+        type = int, 
+        default = 10,
+        help = "percentage *****"
+    )
+
+    parser.add_argument(
+        '--save_model', 
+        action='store_true',
+        help = 'save model parameters'
+    )
+
 
     # CBS ARGS
-    parser.add_argument('--std', default=1, type=float)
-    parser.add_argument('--std_factor', default=0.9, type=float)
-    parser.add_argument('--epoch', default=5, type=int)
-    parser.add_argument('--kernel_size', default=3, type=int)
+    parser.add_argument(
+        '--std', 
+        type = float,
+        default = 1,
+        help = 'standard deviation'
+    )
 
-    #Our ARGS
-    parser.add_argument('--epoch_limit',type=int,default=200)
-    parser.add_argument('--precision_point', type=int, default=2)
+    parser.add_argument(
+        '--std_factor',
+        type = float,
+        default = 0.9,
+    )
 
+    parser.add_argument(
+        '--epoch', 
+        type = int,
+        default = 5, 
+    )
+    
+    parser.add_argument(
+        '--kernel_size', 
+        type=int,
+        default=3,
+        help = 'kernel size (square)'
+    )
 
-    #2Kernel Set ARGS
-    parser.add_argument('--kernel_type', default=13, type=int)
+    # OUR ARGS
+    parser.add_argument(
+        '--epoch_limit',
+        type=int,
+        default = 200
+    )
+
+    parser.add_argument(
+        '--precision_point', 
+        type = int, 
+        default = 2
+    )
+
+    # 2Kernel Set ARGS
+    parser.add_argument(
+        '--kernel_type', 
+        type = int,
+        default = 13,
+        help = 'kernel configuration type'
+    )
+    # ---------------------------------------------------------------------------------------------
 
     args = parser.parse_args()
 
-    args.cuda = True if torch.cuda.is_available() and not args.no_cuda else False
+    args.cuda = (not args.no_cuda and torch.cuda.is_available())
 
     return args
 

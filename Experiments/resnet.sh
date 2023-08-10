54a6ee59f01a451401c7556d1c8634f79b24367c
theokoles <gabrieltrahan777@hotmail.com>
2023-08-10 13:44:52 -0500

    Creates experiments w/o caltech and imagenet

---
+++ Experiments/resnet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##################################################################
+# RUN NORMALL CNN ON EACH DATASET, DISTRIBUTION, AND KERNEL TYPE #
+##################################################################
+
+# For each dataset, ...
+for dataset in cifar10 cifar100 mnist svhn
+do
+    # distribution, ...
+    for distribution in cauchy gaussian gumbel laplace poisson
+    do
+        # and kernel type, ...
+        for kernel_type in $(seq 1 14);
+        do
+            # Run an experiment.
+            python main.py \
+                --model resnet \
+                --dataset $dataset \
+                --distribution $distribution \
+                --kernel_type $kernel_type
+        done
+    done
+done

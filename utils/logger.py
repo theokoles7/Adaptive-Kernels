28abb6cf7dd4f89864f009422e6afafc2abb1c23
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-10-23 09:00:46 -0500

    Modifies logging paths

--- utils/logger.py
+++ utils/logger.py
@@ -11,7 +11,7 @@ LOGGER = logging.getLogger('hi-lo')
 LOGGER.setLevel(logging.DEBUG)
 
 # Ensure that output ARGS.logger_path exists
-os.makedirs(ARGS.logger_path, exist_ok=True)
+os.makedirs(f"{ARGS.logger_path}/{ARGS.distribution}{f'/{ARGS.kernel_type}' if ARGS.distribution else ''}/{ARGS.dataset}", exist_ok=True)
 
 # Define console handler
 stdout_handler = logging.StreamHandler(sys.stdout)
@@ -20,7 +20,7 @@ stdout_handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(n
 LOGGER.addHandler(stdout_handler)
 
 # Define file handler
-file_handler = logging.FileHandler(f"{ARGS.logger_path}/{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log")
+file_handler = logging.FileHandler(f"{ARGS.logger_path}/{ARGS.distribution}{f'/{ARGS.kernel_type}' if ARGS.distribution else ''}/{ARGS.dataset}/{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log")
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(name)s : %(message)s'))
 LOGGER.addHandler(file_handler)

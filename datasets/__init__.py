ed723f3e18d464040dbd9983bacad80588c557bf
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-10-23 10:53:48 -0500

    Adds ImageNet dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,9 @@
-__all__ = ['accessor', 'cifar10', 'cifar100', 'mnist', 'svhn']
+__all__ = ['accessor', 'cifar10', 'cifar100', 'imagenet', 'mnist', 'svhn']
 
 from datasets.accessor  import get_dataset
 
 from datasets.cifar10   import Cifar10
 from datasets.cifar100  import Cifar100
+from datasets.imagenet  import ImageNet
 from datasets.mnist     import MNIST
 from datasets.svhn      import SVHN

f6bf8acba1b372a9b602b2a2968807760f34fc87
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-10-28 10:50:00 -0500

    Typo

--- utils/logger.py
+++ utils/logger.py
@@ -20,7 +20,7 @@ stdout_handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(n
 LOGGER.addHandler(stdout_handler)
 
 # Define file handler
-file_handler = logging.FileHandler(f"{ARGS.logger_path}/{ARGS.distribution}{f'/{ARGS.kernel_type}' if ARGS.distribution else ''}/{ARGS.dataset}/{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log")
+file_handler = logging.FileHandler(f"{ARGS.logger_path}/{ARGS.model}/{ARGS.distribution}{f'/{ARGS.kernel_type}' if ARGS.distribution else ''}/{ARGS.dataset}/{datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log")
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(name)s : %(message)s'))
 LOGGER.addHandler(file_handler)

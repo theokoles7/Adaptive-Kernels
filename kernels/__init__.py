c43dc81f2447e24cc33a9083e9c38fd4c6e07168
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-12-22 12:01:28 -0600

    Begins another rewrite

--- kernels/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ['accessor', 'cauchy', 'gaussian', 'gumbel', 'laplace', 'poisson']
-
-from kernels.accessor   import get_kernel
-
-from kernels.cauchy     import CauchyKernel
-from kernels.gaussian   import GaussianKernel
-from kernels.gumbel     import GumbelKernel
-from kernels.laplace    import LaplaceKernel
-from kernels.poisson    import PoissonKernel

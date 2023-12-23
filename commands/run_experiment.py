0ac86e558e708fd6d48ed2f3fb5096ff64519f8a
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-12-22 18:10:55 -0600

    Implements commands

--- commands/run_experiment.py
+++ commands/run_experiment.py
@@ -11,62 +11,17 @@ def run_experiment() -> None:
     if ARGS.by_model:
         LOGGER.info(f"Executing experiment by model: {ARGS.by_model}")
 
-        match ARGS.by_model:
-
-            case "normal":
-                os.system("./experiments/by_model/normal.sh")
-
-            case "resnet":
-                os.system("./experiments/by_model/resnet.sh")
-
-            case "vgg":
-                os.system("./experiments/by_model/vgg.sh")
-
-            case _:
-                raise NotImplementedError(f"Experiment by model not supported for {ARGS.by_model}")
+        os.system(f"./experiments/by_model/{ARGS.by_model}.sh")
 
     elif ARGS.by_kernel:
         LOGGER.info(f"Executing experiment by kernel: {ARGS.by_kernel}")
 
-        match ARGS.by_kernel:
-
-            case "cauchy":
-                os.system("./experiments/by_kernel/cauchy.sh")
-
-            case "gaussian":
-                os.system("./experiments/by_kernel/gaussian.sh")
-
-            case "gumbel":
-                os.system("./experiments/by_kernel/gumbel.sh")
-
-            case "laplace":
-                os.system("./experiments/by_kernel/laplace.sh")
-
-            case "poisson":
-                os.system("./experiments/by_kernel/poisson.sh")
-
-            case _:
-                raise NotImplementedError(f"Experiment by kernel not supported for {ARGS.by_kernel}")
+        os.system(f"./experiments/by_kernel/{ARGS.by_kernel}.sh")
 
     elif ARGS.by_dataset:
         LOGGER.info(f"Executing experiment by dataset: {ARGS.by_dataset}")
 
-        match ARGS.by_dataset:
-
-            case "cifar10":
-                os.system("./experiments/by_dataset/cifar10.sh")
-
-            case "cifar100":
-                os.system("./experiments/by_dataset/cifar100.sh")
-
-            case "imagenet":
-                os.system("./experiments/by_dataset/imagenet.sh")
-
-            case "mnist":
-                os.system("./experiments/by_dataset/mnist.sh")
-
-            case _:
-                raise NotImplementedError(f"Experiment by dataset not supported for {ARGS.by_dataset}")
+        os.system(f"./experiments/by_dataset/{ARGS.dataset}.sh")
             
     elif ARGS.control:
         LOGGER.info("Executing control experiments")

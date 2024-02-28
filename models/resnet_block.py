a62c0a85309519e8459de811049aa0ca32bd104a
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2024-02-28 09:04:42 -0600

    Fixes variable spelling

--- models/resnet_block.py
+++ models/resnet_block.py
@@ -22,8 +22,8 @@ class ResnetBlock(nn.Module):
         super(ResnetBlock, self).__init__()
 
         # Initialize distribution parameters
-        self.location =     [ARGS.location if ARGS.distribtion != "poisson" else ARGS.rate]*2
-        self.scale =        [ARGS.scale]*2
+        self.location =     [(ARGS.location if ARGS.distribution != "poisson" else ARGS.rate) if ARGS.distribution else 0.0]*2
+        self.scale =        [ARGS.scale if ARGS.distribution else 1.0]*2
 
         self.channels_out = channels_out
 
@@ -38,51 +38,55 @@ class ResnetBlock(nn.Module):
         # Shortcut layer
         self.shortcut =     nn.Sequential()
 
-        # if stride != 1 or channels_in != (channels_out):
-        #     self.shortcut_kernel = True
-        #     self.shortcut = nn.Sequential(
-        #         nn.Conv2d(channels_in, (channels_out), kernel_size=1, stride=stride, bias=False),
-        #         nn.BatchNorm2d(channels_out)
-        #     )
+        if stride != 1 or channels_in != (channels_out):
+            self.shortcut_kernel = True
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(channels_in, channels_out, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(channels_out)
+            )
 
-        def forward(self, X: torch.Tensor) -> torch.Tensor:
-            """Feed input through network and provide output.
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        """Feed input through network and provide output.
 
-            Args:
-                X (torch.Tensor): Input tensor
+        Args:
+            X (torch.Tensor): Input tensor
 
-            Returns:
-                torch.Tensor: Output tensor
-            """
-            # INPUT LAYER =========================================================================
-            self._logger.debug(f"Input shape: {X.shape}")
+        Returns:
+            torch.Tensor: Output tensor
+        """
+        # INPUT LAYER =========================================================================
+        self._logger.debug(f"Input shape: {X.shape}")
 
-            # LAYER 1 =============================================================================
-            x1 = self.conv1(X)
+        # LAYER 1 =============================================================================
+        x1 = self.conv1(X)
 
-            self._logger.debug(f"Layer 1 shape: {x1.shape}")
+        self._logger.debug(f"Layer 1 shape: {x1.shape}")
 
-            with torch.no_grad(): y, self.location[0], self.scale[0] = x1.float(), torch.mean(y).item(), torch.std(y).item()
+        with torch.no_grad(): 
+            y = x1.float()
+            self.location[0], self.scale[0] = torch.mean(y).item(), torch.std(y).item()
 
-            # LAYER 2 =============================================================================
-            x2 = self.conv2(F.relu(self.bn1(self.kernel1(x1) if ARGS.distribution else x1)))
+        # LAYER 2 =============================================================================
+        x2 = self.conv2(F.relu(self.bn1(self.kernel1(x1) if ARGS.distribution else x1)))
 
-            self._logger.debug(f"Layer 2 shape: {x2.shape}")
+        self._logger.debug(f"Layer 2 shape: {x2.shape}")
 
-            with torch.no_grad(): y, self.location[1], self.scale[1] = x2.float(), torch.mean(y).item(), torch.std(y).item()
+        with torch.no_grad(): 
+            y = x2.float()
+            self.location[1], self.scale[1] = torch.mean(y).item(), torch.std(y).item()
 
-            # OUTPUT LAYER ========================================================================
-            output = self.bn2(self.kernel2(x2) if ARGS.distribution else x2)
+        # OUTPUT LAYER ========================================================================
+        output = self.bn2(self.kernel2(x2) if ARGS.distribution else x2)
 
-            self._logger.debug(f"Output layer shape: {output.shape}")
+        self._logger.debug(f"Output layer shape: {output.shape}")
 
-            # SHORTCUT LAYER ======================================================================
-            output += self.shortcut(output)
+        # SHORTCUT LAYER ======================================================================
+        # output += self.shortcut(output)
 
-            # Return output
-            return F.relu(output)
-        
-        def set_kernels(self) -> None:
-            """Create/update kernels."""
-            self.kernel1 = get_kernel(ARGS.distribution, ARGS.kernel_size, self.channels_out, location=self.location[0], scale=self.scale[0], rate=self.location[0])
-            self.kernel2 = get_kernel(ARGS.distribution, ARGS.kernel_size, self.channels_out, location=self.location[1], scale=self.scale[1], rate=self.location[1])
+        # Return output
+        return F.relu(output)
+    
+    def set_kernels(self) -> None:
+        """Create/update kernels."""
+        self.kernel1 = get_kernel(ARGS.distribution, ARGS.kernel_size, self.channels_out, location=self.location[0], scale=self.scale[0], rate=self.location[0])
+        self.kernel2 = get_kernel(ARGS.distribution, ARGS.kernel_size, self.channels_out, location=self.location[1], scale=self.scale[1], rate=self.location[1])
